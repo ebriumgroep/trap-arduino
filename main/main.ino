89d4db0eca6659ab794d7ee0f52662ecedca719e
Pieter Schoeman <pieter@erbium.co.za>
2018-06-26 19:26:05 +0200

    Added a way to coordinate different tasks according to time

--- main/main.ino
+++ main/main.ino
@@ -1,16 +1,45 @@
-//Import our libraries
-#include "src/dht_library/DHT.h"
-#include "src/gsm_library/GSM.h"
+//Import our Libraries
+#include "src/dht_library/DHT.h"    // Temperature and Humidity Sensor
+#include "src/gsm_library/GSM.h"    // GSM Modem
+
+// Import Temprorary Standard Libraries
+#include <Time.h>
 
 GSM *gsm;
 DHT dht(4, 22);
 
+// The state of the system
+enum state
+{
+  SENSING_GENR, // Sensing General
+  SENSING_TEMP, // Sensing Temperature and Humidity
+  TRANSMITTING  // Transmitting Data
+};
+state control = SENSING_GENR;
+
+// Sensorr Constants
+const int SENSOR = 6;
+
+// Variables
+int start = day();
+
+// This should all be dynamically allocated in the setup function from the settings textfile
+// Only for current testing purposes
+int arrReadTemp[] = {1, 2, 3, 4, 5};
+int arrReadCount[] = {0, 0, 0, 0, 0};
+int arrTransData[] = {10};
+int arrTransCount[] = {0};
+
 void setup()
 {
-	// Starts the Serial port for debugging
+	// Starts the Serial connection for debugging
 	Serial.begin(9600);
 
+  // Points the GSM pointer to the GSM object
   gsm = new GSM(2, 3);
+
+  // Initialises the input pins
+  pinMode(SENSOR, INPUT);
 
 	// Starts the temperature sensor
 	dht.begin();
@@ -29,22 +58,109 @@ void setup()
 }
 
 void loop()
-{
-	// Wait for the sensor
-	delay(2000);
-
-	// Read the temperature and assign it to the GSM modem
-	float t = dht.readTemperature();
-  float h = dht.readHumidity();
+{ 
+  // The current minute for debugging
+  Serial.println(String("Current Minute: ")+String(minute()));
   
-  String data = String("Date,")+String("Time,")+String("Trap-ID,")+String(t)+String(",")+String(h);
-  int hash = gsm->hash(data);
+  // Is it time to sense for temperature
+  for(int a=0; a<5; a++)
+  {
+    if((minute() == arrReadTemp[a]) and (arrReadCount[a] == 0))
+    {
+      Serial.println("T&H Active");
+      control = SENSING_TEMP;
+      arrReadCount[a] = 1;
+      break;  
+    }
+  }
 
-	gsm->setMessage(data);  // Compare the returned hash with the locally generated hash.
+  // Is it time to transmit data
+  for(int a=0; a<1; a++)
+  {
+    if((minute() == arrTransData[a]) and (arrTransCount[a] == 0))
+    {
+      Serial.println("Transmit Active");
+      control = TRANSMITTING;
+      arrTransCount[a] = 1;
+      break;
+    }
+  }
 
-	// Use the GSM modem to post the temperature to the server
-	if (gsm->postRequest())
-	{
-		Serial.println("Temperature Posted Successfully");
-	}
+  // Execute the tasks
+  switch(control)
+  {
+    case SENSING_GENR:  
+    {
+      Serial.println("SENSING_GENR");
+      
+      bool state = false;
+      state = digitalRead(SENSOR);
+      if(state)
+      {
+        delay(2000);
+        // -> To Text File 
+      }
+
+      // Delay the sensor for debugging
+      delay(1000);
+      
+      break;
+    }
+    case SENSING_TEMP:
+    {
+      Serial.println("SENSING_TEMP");
+      
+      // Read the temperature/humidity and assign it to the GSM modem
+      float t = dht.readTemperature();
+      float h = dht.readHumidity();
+      
+      // -> To Text File
+      
+      control = SENSING_GENR;
+      break;
+    }
+    case TRANSMITTING:
+    {
+      Serial.println("TRANSMITTING");
+      
+      // This will be in an while loop reading from the entries in the files.
+      String data = "test data";
+      int hash = gsm->hash(data);
+
+      gsm->setMessage(String(hash)+String(",")+String(data));
+
+      // Use the GSM modem to post the temperature to the server
+      if (gsm->postRequest())
+      {
+        // Reads the returned data from the post request to determine if the transmission was sucsessfull
+        if(true)
+        {
+          Serial.println("Data Successfully Transmitted");
+          control = SENSING_GENR;
+        }
+        else
+        {
+          Serial.println("Something went wrong in transmission, trying again");
+          control = TRANSMITTING;
+        }
+      }
+      break;     
+    }
+  }
+
+  if(day() != start)
+  {
+    
+    Serial.println("Counters Reset");
+    
+    start = day();
+    for(int a=0; a<5; a++)
+    {
+      arrReadCount[a] = 0;
+    }
+    for(int a=0; a<1; a++)
+    {
+      arrTransCount[a] = 0;
+    }
+  }
 }
